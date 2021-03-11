51753f0d0c2a512ca73d1c5d928537a5f9aed2c8
Manuel Sabogal <mfer32@gmail.com>
2021-03-11 02:23:55 -0500

    Added debug support and executable icon

--- build.py
+++ build.py
@@ -1,14 +1,20 @@
 import compileall
 import os
 import os.path
-import re
 import shutil
 import subprocess
 import sys
-import PyInstaller
 
 source_dir = os.path.dirname(__file__)
 build_dir = os.path.join(source_dir, "build")
+
+######################################################################
+
+APP_NAME = "edobot"
+APP_ICON = os.path.join(source_dir, "www", "favicon.ico")
+
+######################################################################
+
 
 if not os.path.exists(build_dir):
     os.makedirs(build_dir)
@@ -17,12 +23,10 @@ os.chdir(source_dir)
 
 python_exe = sys.executable
 
-optimized = 2
+optimized = 0
 if optimized == 0:
-    optimized_flag = ""
     optimized_prefix = ""
 else:
-    optimized_flag = "-" + "O" * optimized
     optimized_prefix = f".opt-{optimized}"
 
 py_cache_prefix = f".{sys.implementation.cache_tag}{optimized_prefix}.pyc"
@@ -46,24 +50,29 @@ compileall.compile_dir(components_dir, optimize=optimized)
 print("======================== Creating executable  ========================")
 
 os.chdir(build_dir)
-subprocess.run([
-    python_exe,
-    optimized_flag,
-    "-m",
-    "PyInstaller",
-    "-F",
-    # "--hidden-import=obswebsocket",
-    os.path.join(source_dir, "src", "main.py")
-])
+pyinstaller_exec = [python_exe, "-m", "PyInstaller"]
+hidden_imports = []
+if optimized:
+    pyinstaller_exec.insert(1, "-" + "O" * optimized)
+else:
+    # pyinstaller_exec.append("--debug=bootloader")
+    pyinstaller_exec.append("--debug=noarchive")
+    hidden_imports.append("xmlrpc.server")
+    hidden_imports.append("site")
+subprocess.run(
+    pyinstaller_exec + [f"--hidden-import={x}" for x in hidden_imports] +
+    ["-F", f"--name={APP_NAME}",  f"--icon={APP_ICON}",
+     os.path.join(source_dir, "src", "main.py")]
+)
 
 print("======================== Copying data         ========================")
 
-dest_dir = os.path.join(build_dir, "dist")
+dist_dir = os.path.join(build_dir, "dist")
 shutil.copytree(os.path.join(source_dir, "www"),
-                os.path.join(dest_dir, "www"), dirs_exist_ok=True,
+                os.path.join(dist_dir, "www"), dirs_exist_ok=True,
                 copy_function=copy_function)
 shutil.copytree(components_pycache,
-                os.path.join(dest_dir, "components"), dirs_exist_ok=True,
+                os.path.join(dist_dir, "components"), dirs_exist_ok=True,
                 copy_function=copy_function,
                 ignore=shutil.ignore_patterns("*.py"))
 
@@ -85,8 +94,10 @@ print("===================== Copying required modules =======================")
 requirements_lib_folder = os.path.join(pip_install_dir, "Lib", "site-packages")
 compileall.compile_dir(requirements_lib_folder, legacy=True, optimize=optimized)
 
-module_dest_dir = os.path.join(dest_dir, "modules")
-shutil.copytree(requirements_lib_folder, module_dest_dir,
+module_dist_dir = os.path.join(dist_dir, "modules")
+shutil.copytree(requirements_lib_folder, module_dist_dir,
                 dirs_exist_ok=True, copy_function=copy_function,
                 ignore=shutil.ignore_patterns("tests", "__pycache__",
                                               "*.dist-info", "*.py"))
+
+shutil.make_archive(APP_NAME, 'zip', dist_dir)

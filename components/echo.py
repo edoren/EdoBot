34ebeab1f18896389760bac7817c7d08efc38f7b
Manuel Sabogal <mfer32@gmail.com>
2021-04-06 17:47:53 -0500

    Changed echo component to print data to the logs

--- components/echo.py
+++ components/echo.py
@@ -1,10 +1,12 @@
-from datetime import datetime
+import logging
 from typing import Any, List, Mapping, Optional, Set, Union
 
 from model import User
 from twitch import ChatComponent, UserType
 
 __all__ = ["EchoComponent"]
+
+gLogger = logging.getLogger("edobot.components.echo")
 
 
 class EchoComponent(ChatComponent):  # TODO: Change to chat store
@@ -18,23 +20,25 @@ class EchoComponent(ChatComponent):  # TODO: Change to chat store
 
     @staticmethod
     def get_description() -> str:
-        return "This component just prints the chat"
+        return "This component just displays the chat in the logs"
 
     def get_command(self) -> Optional[Union[str, List[str]]]:
         return None  # To get all the messages without command filtering
 
     def start(self) -> None:
+        gLogger.info("Starting Echo component")
         super().start()
 
     def stop(self) -> None:
+        gLogger.info("Stopping Echo component")
         super().stop()
 
     def process_message(self, message: str, user: User, user_types: Set[UserType]) -> bool:
-        print(f"[{datetime.now().strftime('%d/%m/%Y %H:%M:%S')}] [{user.display_name}] {message}")
+        gLogger.info((f"[{user.display_name}] {message}"))
         return True
 
     def process_event(self, event_name: str, payload: Mapping[str, Any]) -> bool:
         user = payload["redemption"]["user"]["display_name"]
         reward_name = payload["redemption"]["reward"]["title"]
-        print(f"[{datetime.now().strftime('%d/%m/%Y %H:%M:%S')}] [{user}] {event_name}: {reward_name}")
+        gLogger.info((f"[{user}] {event_name}: {reward_name}"))
         return True

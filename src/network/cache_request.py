2ee9c1a625a06406ff5ceeba6030dd2bb761273a
Manuel Sabogal <mfer32@gmail.com>
2021-04-09 16:39:18 -0500

    Added caching for Twitch requests

---
+++ src/network/cache_request.py
@@ -0,0 +1,29 @@
+import time
+from typing import Any, Optional
+
+import requests
+
+
+class CacheRequest:
+    def __init__(self, method: str,  url: str, timeout_seconds: int = 5*60, **kwargs: Any):
+        self.method = method
+        self.url = url
+        self.timeout = timeout_seconds * 1000
+        self.expire_time: Optional[int] = None
+        self.cached_response: Optional[requests.Response] = None
+        self.request_kargs = kwargs
+        if method == "GET":
+            self.request_kargs.setdefault('allow_redirects', True)
+        self.request_kargs.setdefault("params", None)
+
+    def set_timeout(self, timeout_seconds: int):
+        self.timeout = timeout_seconds * 1000
+
+    def call(self, force: bool = False) -> requests.Response:
+        current_time = round(time.time() * 1000)
+        if force or self.expire_time is None or self.cached_response is None or current_time > self.expire_time:
+            self.expire_time = current_time + self.timeout
+            self.cached_response = requests.request(self.method, self.url, **self.request_kargs)
+            return self.cached_response
+        else:
+            return self.cached_response
--- src/twitch/service.py
+++ src/twitch/service.py
@@ -1,12 +1,11 @@
 import logging
 import time
-from typing import Any, List, Mapping
-
-import requests
+from typing import List, MutableMapping, Optional
 
 import model
 from core.constants import Constants
 from model.access_token import AccessToken
+from network.cache_request import CacheRequest
 
 __all__ = ["Service"]
 
@@ -23,52 +22,62 @@ class Service:
 
         self.__active = True
 
-        self.user = self.get_users()[0]
-        self.chatters: List[model.User] = []
+        self.users_cache: MutableMapping[str, CacheRequest] = {}
 
-    def __call_endpoint(self, path: str, params: Mapping[str, Any] = {}):
+        self.user = self.get_user()
+
+        self.mod_requestor = self.__get_cache_requestor("GET", "/moderation/moderators",
+                                                        params={"broadcaster_id": self.user.id})
+        self.sub_requestor = self.__get_cache_requestor("GET", "/subscriptions",
+                                                        params={"broadcaster_id": self.user.id})
+
+    def __call_endpoint(self, requestor: CacheRequest):
         retry_time = 1
         while self.__active:
-            request_url = "https://api.twitch.tv/helix" + path
             try:
-                request = requests.get(
-                    request_url,
-                    params=params,
-                    headers={
-                        "Authorization": f"{self.token.token_type.title()} {self.token.access_token}",
-                        "Client-Id": Constants.CLIENT_ID
-                    }
-                )
+                request = requestor.call(False)
                 if request.status_code == 200:
                     return request.json()
                 elif request.status_code == 401:
                     raise UnauthenticatedException("Unauthenticated: Missing/invalid Token")
                 else:
-                    gLogger.error(f"Error reaching url '{request_url}' failed: {request.json()}")
+                    gLogger.error(f"Error reaching url '{request.url}' failed: {request.json()}")
                     return {"data": None}
             except UnauthenticatedException as e:
                 raise e
             except Exception as e:
-                gLogger.error(f"Error reaching url '{request_url}' retying in {retry_time} seconds: {e}")
+                gLogger.error(f"Error reaching url '{requestor.url}' retying in {retry_time} seconds: {e}")
                 time.sleep(retry_time)
                 retry_time *= 2
                 continue
         return {"data": None}
 
     def get_moderators(self) -> List[model.Moderator]:
-        response = self.__call_endpoint("/moderation/moderators", params={"broadcaster_id": self.user.id})
+        response = self.__call_endpoint(self.mod_requestor)
         return [model.Moderator(**x) for x in response["data"] or []]
 
     def get_subscribers(self) -> List[model.Suscription]:
-        response = self.__call_endpoint("/subscriptions", params={"broadcaster_id": self.user.id})
+        response = self.__call_endpoint(self.sub_requestor)
         return [model.Suscription(**x) for x in response["data"] or []]
 
-    def get_users(self, names: List[str] = []) -> List[model.User]:
-        response = self.__call_endpoint("/users", params={"login[]": names})
-        return [model.User(**x) for x in response["data"] or []]
+    def get_user(self, name: Optional[str] = None) -> model.User:
+        if name is None:
+            name = "$"
+            requestor = self.users_cache.setdefault(name, self.__get_cache_requestor("GET", "/users"))
+        else:
+            requestor = self.users_cache.setdefault(name, self.__get_cache_requestor("GET", "/users",
+                                                                                     params={"login": name}))
+        response = self.__call_endpoint(requestor)
+        user = [model.User(**x) for x in response["data"] or []][0]
+        if name == "$":
+            self.users_cache[user.login] = self.users_cache["$"]
+        return user
 
-    def get_chatters(self):
-        pass
+    def __get_cache_requestor(self, method, path, params=None):
+        return CacheRequest(method, "https://api.twitch.tv/helix" + path, params=params, headers={
+            "Authorization": f"{self.token.token_type.title()} {self.token.access_token}",
+            "Client-Id": Constants.CLIENT_ID
+        })
 
     def __del__(self):
         self.__active = False

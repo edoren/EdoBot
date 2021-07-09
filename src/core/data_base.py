63fa8ba4c7db61a9039363aabdccb98262ca956c
Manuel Sabogal <mfer32@gmail.com>
2021-07-09 13:57:00 -0500

    Fixed Database class to contain all the necessary methods

--- src/core/data_base.py
+++ src/core/data_base.py
@@ -12,24 +12,6 @@ class DataBasePrivate:
     def __init__(self, store_dir):
         self.db = TinyDB(os.path.join(store_dir, "db.json"), indent=4)
 
-    def set_user_token(self, user: str, token: AccessToken) -> None:
-        tokens_table = self.db.table("tokens")
-        findings = tokens_table.search(where("user") == user)
-        if len(findings) == 0:
-            tokens_table.insert({"user": user, "token": token.__dict__})
-        else:
-            tokens_table.update({"user": user, "token": token.__dict__}, where("user") == user)
-
-    def get_token_for_user(self, user: str) -> Optional[AccessToken]:
-        tokens_table = self.db.table("tokens")
-        findings = tokens_table.search(where("user") == user)
-        if len(findings) > 0:
-            return AccessToken(**findings[0]["token"])
-
-    def remove_user(self, user: str) -> Optional[AccessToken]:
-        tokens_table = self.db.table("tokens")
-        tokens_table.remove(where("user") == user)
-
 
 class DataBase:
     instance: Optional[DataBasePrivate] = None
@@ -37,6 +19,22 @@ class DataBase:
     def __init__(self, store_dir):
         if not DataBase.instance:
             DataBase.instance = DataBasePrivate(store_dir)
+        self.__db = DataBase.instance.db
 
-    def __getattr__(self, name):
-        return getattr(DataBase.instance, name)
+    def set_user_token(self, user: str, token: AccessToken) -> None:
+        tokens_table = self.__db.table("tokens")
+        findings = tokens_table.search(where("user") == user)
+        if len(findings) == 0:
+            tokens_table.insert({"user": user, "token": token.__dict__})
+        else:
+            tokens_table.update({"user": user, "token": token.__dict__}, where("user") == user)
+
+    def get_token_for_user(self, user: str) -> Optional[AccessToken]:
+        tokens_table = self.__db.table("tokens")
+        findings = tokens_table.search(where("user") == user)
+        if len(findings) > 0:
+            return AccessToken(**findings[0]["token"])
+
+    def remove_user(self, user: str) -> Optional[AccessToken]:
+        tokens_table = self.__db.table("tokens")
+        tokens_table.remove(where("user") == user)
